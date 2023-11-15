8ec6180ea7d0078fc6105d1c89d80b20f9ad29f8
MaskeredeZorro <Alexanderstrarup@gmail.com>
2023-11-15 11:27:29 +0100

    d

---
+++ Lecture 14.R
@@ -0,0 +1,6 @@
+library(tidyverse)
+
+ggplot(data = starwars, mapping = aes(x = height, y = mass)) +
+  geom_point() +
+  labs(title = "Mass vs. height of Starwars characters",
+       x = "Height (cm)", y = "Weight (kg)")
